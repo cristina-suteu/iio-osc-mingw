f2612de1a1498964050f66482b935726b118894a
Cristina Suteu <cristina.suteu@analog.com>
2022-09-14 13:15:05 +0300

    gtk3 migration: added Meson build system added Meson cross compile files added missing deps to appveyor and install_deps script fixed deps versions to be GTK3 compatible
    Signed-off-by: Cristina Suteu <cristina.suteu@analog.com>

--- install_deps.sh
+++ install_deps.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
 apt-get install cmake autoconf automake libtool \
-	libglib2.0-dev libgtk2.0-dev oasis indent \
-	gcc-mingw-w64-i686 gcc-mingw-w64-x86_64 \
-	transfig texinfo
+	libglib2.0-dev libgtk-3-dev oasis indent \
+	gcc-mingw-w64-i686 gcc-mingw-w64-x86-64 \
+	transfig texinfo ninja-build python3 \
+	python3-pip cargo 
+
+pip3 install setuptools
